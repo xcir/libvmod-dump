d017aca9ee2695862a55c6206ac2021c6b954616
Shohei Tanaka(@xcir) <kokoniimasu@gmail.com>
2015-12-11 18:53:52 +0900

    clean

--- src/vmod_dump.c
+++ src/vmod_dump.c
@@ -21,9 +21,12 @@ init_function(const struct vrt_ctx *ctx, struct vmod_priv *priv,
 	return (0);
 }
 
-void dump_VSL_split(struct req *req, char *p, char *p2, unsigned mlen2, const void *ptr, size_t l, unsigned br){
-	size_t length = l;
-	void *c_ptr = (void*)ptr;
+void dump_VSL_split(struct req *req, char *p, unsigned mlen, const void *ptr, size_t l, unsigned br){
+	
+	char *p2       = p + VMOD_DUMP_KEY_LEN;
+	unsigned mlen2 = mlen + VMOD_DUMP_KEY_LEN;
+	size_t length  = l;
+	void *c_ptr    = (void*)ptr;
 	txt t;
 	t.b = p;
 	
@@ -31,7 +34,7 @@ void dump_VSL_split(struct req *req, char *p, char *p2, unsigned mlen2, const vo
 		if(mlen2 > length){
 			memcpy(p2, c_ptr, length);
 			t.e = t.b + length + VMOD_DUMP_KEY_LEN;
-			if(br){
+			if(br) {
 				memcpy(p2 + length, "\r\n", 2);
 				t.e += 2;
 			}
@@ -40,7 +43,7 @@ void dump_VSL_split(struct req *req, char *p, char *p2, unsigned mlen2, const vo
 		}
 		memcpy(p2, c_ptr, mlen2);
 		t.e = t.b + mlen2 + VMOD_DUMP_KEY_LEN;
-		if(br){
+		if(br) {
 			memcpy(p2 + mlen2, "\r\n", 2);
 			t.e += 2;
 		}
@@ -55,10 +58,14 @@ static int __match_proto__(req_body_iter_f)
 vbf_printRequestBody(struct req *req, void *priv, void *ptr, size_t l)
 {
     CHECK_OBJ_NOTNULL(req, REQ_MAGIC);
-	unsigned mlen, mlen2;
+	unsigned mlen;
 	unsigned u;
-	char *p, *p2;
+	char *p;
+	
+	//get vsl max size per line.
 	mlen = cache_param->vsl_reclen - 3;
+	
+	//reserve work-space
 	u = WS_Reserve(req->ws, 0);
 	if(u < mlen) {
 		//no space.
@@ -66,22 +73,23 @@ vbf_printRequestBody(struct req *req, void *priv, void *ptr, size_t l)
 		return (0);
 	}
 	p = req->ws->f;
+	
+	//add prefix
 	strcpy(p, VMOD_DUMP_KEY);
+
+	//val
+	dump_VSL_split(req, p, mlen, priv, strlen(priv), 0);
 	
-	p2 = p + VMOD_DUMP_KEY_LEN;
-	mlen2 = mlen - VMOD_DUMP_KEY_LEN;
-	dump_VSL_split(req,p,p2,mlen2,priv,strlen(priv),0);
-	
-	//header output
+	//request header
 	for(int i=0; i < req->http0->nhd; ++i) {
 		if (req->http0->hd[i].b == NULL && req->http0->hd[i].e == NULL)	continue;
-		dump_VSL_split(req, p, p2, mlen2, req->http0->hd[i].b, (req->http0->hd[i].e - req->http0->hd[i].b), 1);
+		dump_VSL_split(req, p, mlen, req->http0->hd[i].b, (req->http0->hd[i].e - req->http0->hd[i].b), 1);
 	}
-	dump_VSL_split(req,p,p2,mlen2,"\r\n",2,0);
+	dump_VSL_split(req,p,mlen,"\r\n",2,0);
 	
-	//post body
+	//request body
 	if (l > 0){
-		dump_VSL_split(req, p, p2, mlen2, ptr, l, 0);
+		dump_VSL_split(req, p,  mlen, ptr, l, 0);
     }
 	//free work-space
 	WS_Release(req->ws, 0);
